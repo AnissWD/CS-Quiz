9db1117cd7ef54410b1f37e2a60a686922c948d5
Aniss <anisshach2005@gmail.com>
2026-01-19 18:39:24 +0100

    CS Quiz 1.0

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+public class Main{
+    public static void main(String[] args){
+    Quiz quiz= new Quiz();
+
+        quiz.addQuestion(new Question(
+                "Which layer handles routing in the OSI model?",
+                "Transport",
+                "Network",
+                "Data Link",
+                "Session",
+                'B'
+        ));
+
+        quiz.addQuestion(new Question(
+                "What does CPU stand for?",
+                "Central Process Unit",
+                "Central Processor Utility",
+                "Central Processing Unit",
+                "None of the above",
+                'C'
+        ));
+        quiz.addQuestion(new Question(
+                "SQL is mainly used for?",
+                "Designing UI",
+                "Managing databases",
+                "Networking",
+                "Operating systems",
+                'B'
+        ));
+        quiz.addQuestion(new Question(
+                "Which data structure uses FIFO?",
+                "Stack",
+                "Array",
+                "Tree",
+                "Queue",
+                'D'
+        ));
+        quiz.addQuestion(new Question(
+                "What is the binary of decimal 5?",
+                "101",
+                "111",
+                "100",
+                "110",
+                'A'
+        ));
+        quiz.start();
+}}
